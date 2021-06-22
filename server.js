a5c31f1280536f09902fb7c6254288f9e994d526
BrianaAus <84462609+BrianaAus@users.noreply.github.com>
2021-06-22 12:31:34 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-const http = require("http");
+const http = require("http"); // allows your server to make use of HTTP methods, or allows your server to respond to request
 
 // YOUR NODE SERVER CODE GOES HERE
+const PORT = 3000
+const HOST = "127.0.0.1"
+//create the server 
+//http.createServer --> invoking the create server method on the http module
+//takes two arguments
+const server = http.createServer((request, response) => {
+    response.end("Hello Galvanize"); //server started successfully 
+
+});
+
+//invoke the listen() method on the server you created
+//few arguments: a PORT, a HOST(localhost), and a callbackfunction
+server.listen(PORT, HOST, () => {
+    console.log(`Server running at ${HOST}:${PORT}`); 
+  });
+//http://127.0.0.1:3000/
